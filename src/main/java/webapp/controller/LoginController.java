0139db2a5166a2f2972560b075851de8801bc2b9
dangerous1990 <dangerouslm1990@gmail.com>
2015-03-11 15:08:13 +0800

    webapp

---
+++ src/main/java/webapp/controller/LoginController.java
@@ -0,0 +1,24 @@
+package webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import webapp.model.User;
+
+/**
+ * 登陆控制器
+ * @author admin
+ *
+ */
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+	@RequestMapping(value="/login",method=RequestMethod.POST)
+	public String login(@RequestBody User user){
+		System.out.println(user);
+		return "main";
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"
+	pageEncoding="utf-8"%>
+<%@ include file="/include.jsp"%>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>hello</title>
+</head>
+<body>
+	<form action="${ctx}/login/login" method="post">
+
+		用户名<input name="name" type="text" /> <br> 密码 <input
+			name="password" type="text" /> <br> <input type="submit">
+
+	</form>
+</body>
+</html>
