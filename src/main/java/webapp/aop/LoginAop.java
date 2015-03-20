df544811fe13e8531a12442f59a12a73a3b19aab
dangerous1990 <dangerouslm1990@gmail.com>
2015-03-20 15:04:13 +0800

    调整权限拦截

--- src/main/java/webapp/aop/LoginAop.java
+++ src/main/java/webapp/aop/LoginAop.java
@@ -1,8 +1,6 @@
 package webapp.aop;
 
-import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
