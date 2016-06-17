9d522dac4e00e01128240bf7cee9c0f5807a1ff8
dangerous1990 <dangerouslm1990@gmail.com>
2016-06-17 13:44:49 +0800

    集成 swagger ui

--- src/main/java/webapp/controller/LoginController.java
+++ src/main/java/webapp/controller/LoginController.java
@@ -1,8 +1,13 @@
 package webapp.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
 
 import webapp.model.User;
 
@@ -14,9 +19,11 @@ import webapp.model.User;
  */
 @Controller
 @RequestMapping("/login")
+@Api("user")
 public class LoginController {
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String login(User user) {
+	@RequestMapping(value = "/login", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiOperation(value = "登录", httpMethod = "GET", response = User.class, notes = "登录")
+	public String login(@RequestBody User user) {
 		return "main";
 	}
 
