60088d04f21f7454862775719f0d9f4ada58b4c3
dangerous1990 <dangerouslm1990@gmail.com>
2016-07-28 16:12:58 +0800

    集成redis

--- src/test/java/webapp/web/BaseControllerTest.java
+++ src/test/java/webapp/web/BaseControllerTest.java
@@ -1,10 +1,13 @@
 package webapp.web;
 
+import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @WebAppConfiguration
-@ContextConfiguration(locations = { "classpath*:applicationContext-mvc.xml", "classpath*:applicationContext.xml","classpath*:applicationContext-security.xml"})
+@ContextConfiguration(locations = { "classpath*:applicationContext-mvc.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
 public class BaseControllerTest {
 
 }
