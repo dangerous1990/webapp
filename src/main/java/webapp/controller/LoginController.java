03b07dfed9d8a15391bc5957d6a5a91e71d324c5
dangerous1990 <dangerouslm1990@gmail.com>
2016-09-06 14:46:49 +0800

    redis

--- src/main/java/webapp/controller/LoginController.java
+++ src/main/java/webapp/controller/LoginController.java
@@ -12,18 +12,18 @@ import webapp.model.User;
 
 /**
  * 登陆控制器
- * 
+ *
  * @author admin
- * 
  */
 @Controller
 @RequestMapping("/login")
 @Api("user")
 public class LoginController {
-	@RequestMapping(value = "/login", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	@ApiOperation(value = "登录", httpMethod = "GET", response = User.class, notes = "登录")
-	public String login(@RequestBody User user) {
-		return "main";
-	}
+    @RequestMapping(value = "/login", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "登录", response = User.class, notes = "登录")
+    public String login(@RequestBody User user) {
+
+        return "main";
+    }
 
 }
--- src/test/java/webapp/redis/RedisTest.java
+++ src/test/java/webapp/redis/RedisTest.java
@@ -10,6 +10,7 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
 
 import webapp.BaseTest;
 
@@ -38,6 +39,20 @@ public class RedisTest extends BaseTest {
                 }
             }
         });
+        
+        Assert.assertEquals(value, "helloworld");
+    }
+    @Test
+    public void testGet() {
+        redisTemplate.execute(new RedisCallback<String>() {
+            @Override
+            public String doInRedis(RedisConnection connection) throws DataAccessException {
+                connection.set("test1".getBytes(), "helloworld".getBytes());
+                return null;
+            }
+        });
+        
+        String value = redisTemplate.opsForValue().get("test1");
         
         Assert.assertEquals(value, "helloworld");
     }
