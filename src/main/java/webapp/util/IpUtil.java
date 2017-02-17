a5da5aa015951d09afb3eb3a8cefb97c40dd82f2
dangerous1990 <dangerouslm1990@gmail.com>
2017-02-17 13:38:40 +0800

    iputil

---
+++ src/main/java/webapp/util/IpUtil.java
@@ -0,0 +1,18 @@
+package webapp.util;
+
+/**
+ * Created by Administrator on 2017/2/16.
+ */
+public class IpUtil {
+    public static int ipToInt(String ip) {
+        String[] ips = ip.split("\\.");
+        return (Integer.parseInt(ips[0]) << 24) + (Integer.parseInt(ips[1]) << 16)
+                + (Integer.parseInt(ips[2]) << 8) + Integer.parseInt(ips[3]);
+    }
+
+    public static String intToIp(int i) {
+        return ((i >> 24) & 0xFF) + "." + ((i >> 16) & 0xFF) + "."
+                + ((i >> 8) & 0xFF) + "." + (i & 0xFF);
+    }
+
+}
